dafa8920bce7afb3bcf2390ba19244a7d300e19b
VivasvanVerma <vvofficial.yt@gmail.com>
2025-09-03 18:19:54 +0530

    ball

---
+++ ball.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+green = (0, 255, 0)
+pygame.draw.circle(screen, green, (300, 300), 50)
+pygame.draw.circle(screen, green, (100, 100), 50, 3)
+
+pygame.display.update()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+
+pygame.quit()
